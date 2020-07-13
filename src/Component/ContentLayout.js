2e5f8accd3000b0ab066ba22839aa932877d3ec9
jerryn0820 <jnguyen@mavericksoftware.com>
2020-07-13 14:57:36 -0500

    JTN-000 initial commmit with react app

---
+++ src/Component/ContentLayout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ContentLayout extends Component {
+    render() {
+        return (
+        <div className="app-contentLayout">
+            <p>
+                Come back later.
+            </p>
+        </div>
+        );
+    }
+}
+export default ContentLayout;
