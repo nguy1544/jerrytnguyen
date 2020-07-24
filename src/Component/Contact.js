2eaba881671c1d5b7b371d8b18fc13224d8c41ba
DESKTOP-1FS37BH\jerrythenguyen <jerryn0820@gmail.com>
2020-07-23 21:03:17 -0500

    updated padding

--- src/Component/Contact.js
+++ src/Component/Contact.js
@@ -5,10 +5,10 @@ class Contact extends Component {
     render() {
         return (
             <div className="app-contact">
-                <h4>CONTACT</h4>
+                <h2>CONTACT</h2>
                 <ul className="actions">
-                    <li><a className="fa fa-linkedin fa-4x" target="_blank" href="https://www.linkedin.com/in/jerrytnguyen/" /></li>
-                    <li><a className="fa fa-google fa-4x" href="mailto:nguye1544@gmail.com" /></li>
+                    <li><a className="fa fa-linkedin fa-2x" target="_blank" href="https://www.linkedin.com/in/jerrytnguyen/" /></li>
+                    <li><a className="fa fa-google fa-2x" href="mailto:nguye1544@gmail.com" /></li>
                 </ul>
             </div>
         );
