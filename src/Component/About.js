42cb5c53447a6c78caa90375f4906441c82b6b47
Jerry Nguyen <nguy1544@umn.edu>
2024-09-26 12:40:49 -0500

    update profile and resume

--- src/Component/About.js
+++ src/Component/About.js
@@ -10,9 +10,14 @@ class About extends Component {
                 </span>
                 <ul className="list">
                     <li><h2>Jerry Nguyen</h2></li>
-                    <li>Consultant (Software Engineer)</li>
-                    <li><a href="https://objectpartners.com" target="_blank">Object Partners</a></li>
+                    <li>Sr. Software Engineer</li>
                 </ul>
+
+                <span>
+                    Seasoned software engineer with over 7 years of experience and a versatile background in front-end, back-end, and full-stack development, as well as data engineering.
+                    Adept at modernizing applications, crafting secure features, and mentoring junior engineers.
+                    My broad skill set enables me to tackle complex projects and deliver robust solutions across the full software development lifecycle.
+                </span>
             </div>
         );
     }
